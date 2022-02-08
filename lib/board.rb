0c4647c446a77efe4881fe2412d8e0feafc95b1b
Joseph Senecal <jasenecal2@gmail.com>
2022-02-07 20:09:15 -0700

    Test: STAGE and add temp  attr_reader / attr_accessor toggle

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,29 @@
 class Board
+  #toggling on attr_accessor for testing purposes only!
+  #comment out before final shipment
 
-def initialize ()
-  #Represents 7 columns, each with 6 rows.
-  #@space[0] to represent column A, @spaces [0[0]] the top-left-most space.
-@spaces = [[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."], [".", ".", ".", ".", ".", "."]]
-end
+  # attr_reader: @spaces
+  attr_accessor: @spaces
 
-def print_board
- puts "ABCDEFG"
-row = 0
-6.times do
-  @spaces.each do |column|
-   print column[row]
- end
- row += 1
- puts "\n"
-end
+  def initialize ()
+    #Represents 7 columns, each with 6 rows.
+    #@space[0] to represent column A, @spaces [0[0]] the top-left-most space.
+    #We'll fill this with x's to represent humnan moves, o's to represent the computer.
+    @spaces = [[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."],[".", ".", ".", ".", ".", "."], [".", ".", ".", ".", ".", "."]]
+  end
 
-end
+  def print_board
+    puts "ABCDEFG"
+    row = 0
+    6.times do
+      @spaces.each do |column|
+        print column[row]
+      end
+    row += 1
+    puts "\n"
+    end
+  end
+
 end
 
 test = Board.new
